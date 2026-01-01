de4ec7466e927e4803da9430877995f26acd2c51
aryprasetyo <ariieelink@gmail.com>
2026-01-01 12:57:32 +0700

    restore vscode 31des2025

--- src/config/constants.js
+++ src/config/constants.js
@@ -2,7 +2,7 @@ import {
   CheckCircle, LogOut, FileText, AlertTriangle, Clock, Briefcase, Calendar 
 } from 'lucide-react';
 
-export const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwbm_sS8hEB1EG-Iykn8jXL-qm2y64f8Kvj93aIYXBvOlvWAJL7dU-J_6n4Zwl0T93Pxg/exec';
+export const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxbQzpsWnU1TfL7aXzn0ZGmPXDVjYcJXhACUxzUOfiPsUJeutnS_pJWvMdqgcDtFuSLYg/exec';
 
 export const TIMEOUT_DURATION = 5 * 60 * 1000; 
 
