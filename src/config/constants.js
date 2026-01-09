bd735fc8595a27a7bde449503112ffeaf9d54ccb
aryprasetyo <ariieelink@gmail.com>
2026-01-09 13:36:18 +0700

    rebuild database

--- src/config/constants.js
+++ src/config/constants.js
@@ -2,7 +2,7 @@ import {
   CheckCircle, LogOut, FileText, AlertTriangle, Clock, Briefcase, Calendar 
 } from 'lucide-react';
 
-export const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyzHZxqXIsmiyqRHaajJrL34Ao-Q6VuKAsZ59VGqB8o8fOlFU8DwcwT0LapbQlQU60IoQ/exec';
+export const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyHp9Hy5tT5aaw34YfFb_PEp2zqDKPeCZrXrTYsQPVi1Z8trFlwG355uFgNUX8kIHGWLw/exec';
 
 export const TIMEOUT_DURATION = 5 * 60 * 1000; 
 
