0cbda1ec09471ba29bc14dadb8ef5ad9a7ce377f
aryprasetyo <ariieelink@gmail.com>
2026-01-10 12:48:31 +0700

    update nama disetujui saat approve

--- src/config/constants.js
+++ src/config/constants.js
@@ -2,7 +2,7 @@ import {
   CheckCircle, LogOut, FileText, AlertTriangle, Clock, Briefcase, Calendar 
 } from 'lucide-react';
 
-export const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyIycHxjytl1bnWbXbrpa_c1_cxhWgA8lJvWh8xQUiVn45HNf8YYbtUNTUjQHzP9lTarg/exec';
+export const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbypkVnfRU-WkSZRifKnTdYkB_FqcIJqlvA7VD2T_WdGwHytIVVnnWsFpTHLpAVy1-2HOw/exec';
 
 export const TIMEOUT_DURATION = 5 * 60 * 1000; 
 
