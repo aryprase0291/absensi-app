b7a7e71929c97c4f9fde40305ba1a6ccdc38f727
aryprasetyo <ariieelink@gmail.com>
2026-01-10 12:29:17 +0700

    update respon penolakan via email

--- src/config/constants.js
+++ src/config/constants.js
@@ -2,7 +2,7 @@ import {
   CheckCircle, LogOut, FileText, AlertTriangle, Clock, Briefcase, Calendar 
 } from 'lucide-react';
 
-export const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbw_Mbz4Xs1z0GRAjlo_Jbm2ciM_FJyqDdMiWQqFymF9qnTR8a7ReK311v7f1jpVvxhoCw/exec';
+export const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyIycHxjytl1bnWbXbrpa_c1_cxhWgA8lJvWh8xQUiVn45HNf8YYbtUNTUjQHzP9lTarg/exec';
 
 export const TIMEOUT_DURATION = 5 * 60 * 1000; 
 
