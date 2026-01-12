82abd569ce6b981cba1101e529861f9454d1e3db
aryprasetyo <ariieelink@gmail.com>
2026-01-12 17:23:28 +0700

    koreksi timestamp add jam

--- src/config/constants.js
+++ src/config/constants.js
@@ -2,7 +2,7 @@ import {
   CheckCircle, LogOut, FileText, AlertTriangle, Clock, Briefcase, Calendar 
 } from 'lucide-react';
 
-export const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzeqC3ef1VpJAoVm3w9gzIwlMaLuEneBITDaqHbNr3iAujbDKXo8RyTsmgL05AuCZkn6Q/exec';
+export const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxOQZ0ZrIQcJTk_NXl0uhODN3EXoLCYTvW-i63mnYcPrjwRea86LSTBhKq3J2-WidTV9Q/exec';
 
 export const TIMEOUT_DURATION = 5 * 60 * 1000; 
 
