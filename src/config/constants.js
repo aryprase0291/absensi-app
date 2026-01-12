35b478afc0f187bdcb3acc512798e9f48dcce6c1
aryprasetyo <ariieelink@gmail.com>
2026-01-12 18:11:38 +0700

    koreksi tgl dan jam lapor hrd

--- src/config/constants.js
+++ src/config/constants.js
@@ -2,7 +2,7 @@ import {
   CheckCircle, LogOut, FileText, AlertTriangle, Clock, Briefcase, Calendar 
 } from 'lucide-react';
 
-export const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbz9kCnVdZIJhYnfKuq2DnHxqp3C5AINMAucghWcJR03peu8IuumlkxbIaRsWIE8_uRRBw/exec';
+export const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzhNaf4AmvjXNqVtcH6hnSgRPR3vM6rusqhiqxUWoli9ZhTDPji6INSvd9zl9dpep4GqA/exec';
 
 export const TIMEOUT_DURATION = 5 * 60 * 1000; 
 
