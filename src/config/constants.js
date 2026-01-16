eaa75e74cab492c62c10c7b39caff1a7f65b3a90
aryprasetyo <ariieelink@gmail.com>
2026-01-16 07:01:25 +0700

    koreksi pop up info hrd v.5

--- src/config/constants.js
+++ src/config/constants.js
@@ -2,7 +2,7 @@ import {
   CheckCircle, LogOut, FileText, AlertTriangle, Clock, Briefcase, Calendar 
 } from 'lucide-react';
 
-export const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxxPZhcyvxGS40duDrncjMllFGXMvYSVAYT-lFyfR2PDkbUNSZqBlH5zbqjpm7Aq5te_w/exec';
+export const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxc1UQtqcYS52dYjuJ-kifzSLPiLyXY3uu4VxGaPTnVIxpQAgM0dJ_BYB84ywAjJX6M2w/exec';
 
 export const TIMEOUT_DURATION = 5 * 60 * 1000; 
 
