66be2eaabb073f10ac39c81296af3eb894c1b8e9
aryprasetyo <ariieelink@gmail.com>
2026-01-19 09:49:52 +0700

    update timestamp di dbabsen

--- src/config/constants.js
+++ src/config/constants.js
@@ -2,7 +2,7 @@ import {
   CheckCircle, LogOut, FileText, AlertTriangle, Clock, Briefcase, Calendar 
 } from 'lucide-react';
 
-export const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwXSmXFS1lcP3eji7FPNJ2G4IJPR8NKuhRp40XMnZCUZ5hxJdU5U0KdAtKBmy7OkO856A/exec';
+export const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzW8_2wqqQlTN7lNvfEYsGywfRujY26Yt8JEtqIObzeOhoOA7JS7iaI3-KlZR8yrZ6TQg/exec';
 
 export const TIMEOUT_DURATION = 5 * 60 * 1000; 
 
