dc705a045b2e033203c8509ece0e048dfcde278b
aryprasetyo <ariieelink@gmail.com>
2026-01-20 08:41:14 +0700

    fixed tombol approve/reject

--- src/config/constants.js
+++ src/config/constants.js
@@ -2,7 +2,7 @@ import {
   CheckCircle, LogOut, FileText, AlertTriangle, Clock, Briefcase, Calendar 
 } from 'lucide-react';
 
-export const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzW8_2wqqQlTN7lNvfEYsGywfRujY26Yt8JEtqIObzeOhoOA7JS7iaI3-KlZR8yrZ6TQg/exec';
+export const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxGfK-O0sL-VeKy89LKglh7PdHpvATKjZtJKGKPMVdtSkglGx7yOmIX2VkmtgE-MMzI0A/exec';
 
 export const TIMEOUT_DURATION = 5 * 60 * 1000; 
 
