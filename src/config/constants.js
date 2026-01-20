b80ec66dcedebfefa4c06f791fb420c0ffa400bb
aryprasetyo <ariieelink@gmail.com>
2026-01-21 06:31:21 +0700

    koreksi pengajuan di tanggal yg sama

--- src/config/constants.js
+++ src/config/constants.js
@@ -2,7 +2,7 @@ import {
   CheckCircle, LogOut, FileText, AlertTriangle, Clock, Briefcase, Calendar 
 } from 'lucide-react';
 
-export const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzlELnhQCtgAuYOvq5xNKX5uemnmnim1VS2L8rRIMDdvj3k3xO6EMMtQCbsW-RYB8S2EA/exec';
+export const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwfBJ5bpkHQTzFb1U-j6mZy82RziTciKvZI_yZQWqem859dbo38jMpSeJOOPJte4bCsTA/exec';
 
 export const TIMEOUT_DURATION = 5 * 60 * 1000; 
 
